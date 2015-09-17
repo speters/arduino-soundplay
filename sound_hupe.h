6d053ef24ac95f882225b20ead5f4f8328e3e6df
Soenke J. Peters <peters__git@opcenter.de>
2015-09-17 15:22:01 +0200

    cleanup

--- sound_hupe.h
+++
@@ -1,8 +0,0 @@
-// hupe sound made by wav2c
-
-// const int sound_hupe_sampleRate = 8000;
-const unsigned int sound_hupe_length = 24;
-
-const unsigned char __attribute__ ((used)) sound_hupe_data[] PROGMEM ={117,
-135, 138, 147, 166, 154, 119, 121, 125, 113, 129, 136, 96, 125, 147, 136, 110, 100, 108, 120, 131, 
-127, 199, 151, };
